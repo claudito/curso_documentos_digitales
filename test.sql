d714b78095561c588e61990d1f4a973b08d8c2ca
claudito <1107000044@upci.edu.pe>
2021-06-10 03:10:41 -0500

    fix

--- test.sql
+++ test.sql
@@ -2,3 +2,6 @@
 
 
 UPDATE `certificados` SET `estado_correo` = '0',estado_pdf=0,url_documento=null
+
+
+"Failed to authenticate on SMTP server with username \"tallerlaravel7notificaciones@gmail.com\" using 3 possible authenticators. Authenticator LOGIN returned Expected response code 235 but got code \"534\", with message \"534-5.7.14 <https://accounts.google.com/signin/continue?sarp=1&scc=1&plt=AKgnsbv\r\n534-5.7.14 lQfDyOcd3ToLbh8q3QPVW5ABWooOrAeYgp2sVgC-_wCLZZg-TzzpcvlUtmNoF6N-lmb1X\r\n534-5.7.14 RF1Ga_dKkqjQrn3D7hw5tLrXlK5GUtJ65tEkZiokEEH95KphxFQ5jzSONmp5Wtvy>\r\n534-5.7.14 Please log in via your web browser and then try again.\r\n534-5.7.14  Learn more at\r\n534 5.7.14  https://support.google.com/mail/answer/78754 p5sm4729058ejm.115 - gsmtp\r\n\". Authenticator PLAIN returned Expected response code 235 but got code \"534\", with message \"534-5.7.14 <https://accounts.google.com/signin/continue?sarp=1&scc=1&plt=AKgnsbv\r\n534
